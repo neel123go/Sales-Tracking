d973f1dac3b19b6a67a8f976df39ed5961561a60
neel123go <ayounpaul1029@gmail.com>
2022-08-25 00:01:54 +0600

    header design completed

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from './Pages/Shared/Header/Header';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello, World!</h1>
-    </div>
+    <>
+      <Header />
+    </>
   );
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,8 +3,21 @@ module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
-  theme: {
-    extend: {},
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+
+          "primary": "#292524",
+          "secondary": "#DC8665",
+          "accent": "#fcccae",
+          "neutral": "#a8a29e",
+          "base-100": "#E8E5EB",
+          "success": "#55C595",
+          "error": "#dc2626",
+        },
+      },
+    ],
   },
   plugins: [require("daisyui")],
 }
